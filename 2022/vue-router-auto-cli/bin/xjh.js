903148ea130b3f4125674abef3186b8f0997abc7
mynameszhazh <1594776259@qq.com>
2022-11-13 16:04:54 +0800

    fix: 删除多余代码

--- 2022/vue-router-auto-cli/bin/xjh.js
+++ 2022/vue-router-auto-cli/bin/xjh.js
@@ -1,25 +1,8 @@
 #!/usr/bin/env node
-// import { program } from "commander";
-// const { program } = require("commander");
 const program = require("commander");
 const init = require("../lib/init.js");
-// import init from "../lib/init.js";
-const packageJson = require("../package.json");
-// console.log(packageJson);
-// console.log("init", init);
 
-// const { promisify } = require("util");
-// const clear = require("clear");
-// const chalk = require("chalk");
-// const figlet = require("figlet");
-// figlet = promisify(figlet);
-// console.log(clear, chalk)
-
-// console.log(promisify, clear, chalk);
-
-// const log = (content) => console.log(chalk.green(content));
-// 指定版本
-program.version(packageJson.version);
+program.version(require("../package.json").version);
 
 program
   .command("init <name>")
