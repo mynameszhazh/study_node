4bbbd214e683f639d4094bf8f604b663d9f39b4c
mynameszhazh <1594776259@qq.com>
2022-11-30 23:43:05 +0800

    feat: 跨域后端处理模式

--- 2022/http/example/request/api.js
+++ 2022/http/example/request/api.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 const app = http.createServer((req, res) => {
   let { url, method } = req;
   console.log(url, method, "method");
+  console.log("cookie:", req.headers.cookie);
   if (method === "GET" && url === "/") {
     fs.readFile("./index.html", (err, data) => {
       if (!err) {
@@ -11,8 +12,22 @@ const app = http.createServer((req, res) => {
         res.end(data);
       }
     });
-  } else if (url === "/api/user") {
+  } else if (method === "GET" && url === "/api/user") {
+    // 处理cookie
+    res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Content-Type", "text/html");
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.setHeader("Set-Cookie", "cookie=val222");
+    res.end(JSON.stringify([{ name: "xjh" }]));
+  } else if (method === "OPTIONS" && url === "/api/user") {
+    // 处理cookie
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    // 处理复杂请求
+    res.writeHead(200, {
+      "Access-Control-Allow-Origin": "http://localhost:3000",
+      "Access-Control-Allow-Headers": "X-Token,Content-Type",
+      "Access-Control-Allow-Methods": "PUT",
+    });
     res.end(JSON.stringify([{ name: "xjh" }]));
   }
 });
